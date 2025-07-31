cd955dd587b146a325735bc333a0dc381508b077
amrrkhaled <amr.khaledsaada@gmail.com>
2025-07-31 18:02:11 +0300

    fixed a bug in TasksScreenUiState

--- app/src/main/java/com/example/progresstracker/ui/TasksScreenUiState.kt
+++ app/src/main/java/com/example/progresstracker/ui/TasksScreenUiState.kt
@@ -4,6 +4,6 @@ import com.example.progresstracker.ui.model.Task
 
 data class TasksScreenUiState (
     val tasks: List<Task> = emptyList(),
-    val isEditing: Boolean = true,
-    val currentTaskId: Int? = 1,
+    val isEditing: Boolean = false,
+    val currentTaskId: Int? = null,
 )
